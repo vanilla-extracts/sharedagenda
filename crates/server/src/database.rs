a9eb5a442280d5ff4c03f54d1cd2cb1803be6a33
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2025-04-15 14:00:23 +0200

    database: implementing database API

--- crates/server/src/database.rs
+++ crates/server/src/database.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use postgres::{Client, NoTls};
 
-use crate::configuration::DatabaseConfiguration;
+use crate::configuration::{Configuration, DatabaseConfiguration, load};
 
 pub struct Database {
     connection: Client,
@@ -27,6 +27,11 @@ impl std::error::Error for DatabaseError<'_> {
 
 impl Database {
     pub fn new() -> Self {
+        let config = match load() {
+            Ok(cfg) => cfg,
+            Err(_) => Configuration::default(),
+        };
+        let database_configuration = config.database;
         let client = Client::connect(
             &format!(
                 "postgres://{}:{}@{}:{}/{}",
@@ -57,59 +62,70 @@ impl Database {
     pub fn setup_database(mut self) -> Result<(), DatabaseError<'static>> {
         let user_success = match self.connection.batch_execute(
             "
-            create table if not exists user(
+            create table if not exists users(
                 uuid varchar(255) primary key,
                 email varchar(50) not null,
                 name varchar(50) not null,
-                password varchar(50) not null,
-            );   
-        ",
+                password varchar(50) not null
+            );",
         ) {
             Ok(_) => true,
-            Err(_) => false,
+            Err(e) => {
+                println!("{e}");
+                false
+            }
         };
 
         let agenda_success = match self.connection.batch_execute(
             "
             create table if not exists agendas(
                 id serial primary key,
-                owner varchar(255) references user(uuid)
-            );
-        ",
+                owner varchar(255) not null references users(uuid)
+            );",
         ) {
             Ok(_) => true,
-            Err(_) => false,
+            Err(e) => {
+                println!("{e}");
+                false
+            }
         };
 
         let events_success = match self.connection.batch_execute(
             "
                 create table if not exists events(
                     id serial primary key,
-                    agenda_id int references agendas(id),
+                    agenda_id int not null references agendas(id),
                     name varchar(255) not null,
                     date_start date not null,
                     date_end date not null
-                )
-        ",
+                );",
         ) {
             Ok(_) => true,
-            Err(_) => false,
+            Err(e) => {
+                println!("{e}");
+                false
+            }
         };
 
         let token_success = match self.connection.batch_execute(
             "
             create table if not exists token(
                 id serial primary key,
-                owner varchar(255) references user(uuid),
+                owner varchar(255) not null references users(uuid),
                 expiration_date date not null
-            );
-        ",
+            );",
         ) {
             Ok(_) => true,
-            Err(_) => false,
+            Err(e) => {
+                println!("{e}");
+                false
+            }
         };
 
-        let _ = self.connection.close();
+        match self.connection.close() {
+            Ok(_) => println!("Connection to database was closed successfully."),
+            Err(e) => println!("Error while closing database: {e}"),
+        }
 
         if user_success && agenda_success && events_success && token_success {
             Ok(())
@@ -120,14 +136,32 @@ impl Database {
         }
     }
 
-    pub fn execute_statement(mut self, sql: &str) -> bool {
+    pub fn execute_statement(mut self, sql: &str) {
         let result = self.connection.batch_execute(sql);
+        match self.connection.close() {
+            Ok(_) => println!("Connection to database was closed successfully."),
+            Err(e) => println!("Error while closing database: {e}"),
+        }
         match result {
-            Ok(_) => true,
+            Ok(_) => println!("Statement was executed successfully."),
             Err(e) => {
-                println!("{e}");
-                false
+                println!("Error while executing statement: {e}");
             }
+        }
+    }
+
+    pub fn execute_multiple_statements(mut self, sql: Vec<&str>) {
+        let mut i = 1;
+        for statement in sql {
+            match self.connection.batch_execute(statement) {
+                Ok(_) => println!("Success for statement {i}"),
+                Err(e) => println!("Error while executing statement: {e}"),
+            }
+            i += 1;
+        }
+        match self.connection.close() {
+            Ok(_) => println!("Connection to the database was closed successfully."),
+            Err(e) => println!("Error while closing database: {e}"),
         }
     }
 }
