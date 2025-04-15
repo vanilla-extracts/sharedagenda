6c66d76d2695b0941336e35b1f52bc902a57f183
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2025-04-15 10:46:01 +0200

    config: implement configuration

---
+++ crates/server/src/configuration.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DatabaseConfiguration {
+    host: String,
+    port: String,
+    user: String,
+    password: String,
+    database: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Configuration {
+    pub database: DatabaseConfiguration,
+    pub listen_address: String,
+    pub listen_port: i64,
+}
+
+impl Default for DatabaseConfiguration {
+    fn default() -> Self {
+        Self {
+            host: "localhost".to_string(),
+            port: "5432".to_string(),
+            user: "postgres".to_string(),
+            password: "postgres".to_string(),
+            database: "agenda".to_string(),
+        }
+    }
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            database: DatabaseConfiguration::default(),
+            listen_address: "0.0.0.0".to_string(),
+            listen_port: 8000,
+        }
+    }
+}
+
+pub fn write_config(c: &Configuration) -> Result<(), confy::ConfyError> {
+    confy::store("sharedagenda", Some("config"), c)?;
+    Ok(())
+}
+
+pub fn write_default_config() -> Result<(), confy::ConfyError> {
+    write_config(&Configuration::default())
+}
+
+pub fn load() -> Result<Configuration, confy::ConfyError> {
+    let res = confy::load("sharedagenda", Some("config"))?;
+    Ok(res)
+}
