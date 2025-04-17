4b680a1a3e89ba699700f9e3b09ab8317e584a05
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2025-04-17 09:22:46 +0200

    user: implementing logout

--- crates/server/src/users/logout.rs
+++ crates/server/src/users/logout.rs
@@ -1,6 +1,24 @@
+use rocket::{http::Status, serde::json::Json};
+use serde::{Deserialize, Serialize};
+
+use crate::database::Database;
+
 extern crate rocket;
 
-#[get("/user/logout")]
-pub fn logout() -> &'static str {
-    "Hello, World!"
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct UserLogout<'r> {
+    token: &'r str,
+}
+
+async fn delete_token(token: &str) {
+    let db = Database::new().await;
+    db.execute_statement(&format!("delete from token where token='{}'", token))
+        .await;
+}
+
+#[post("/user/logout", format = "application/json", data = "<body>")]
+pub async fn logout(body: Json<UserLogout<'_>>) -> Status {
+    delete_token(body.token).await;
+    Status { code: 200 }
 }
