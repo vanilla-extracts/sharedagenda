e265dcb3433591e7b122ec62d497467a15165525
Charlotte Thomas <me@charlotte-thomas.me>
2025-04-23 16:33:04 +0200

    client: handling deletion

---
+++ crates/cli/src/handlers/delete.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{API_URL, TOKEN};
+
+use super::login::{Answer, call};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DeletePost<'r> {
+    token: &'r str,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DeleteAnswer {
+    code: u16,
+    body: String,
+}
+
+impl Answer for DeleteAnswer {
+    fn code(&self) -> i32 {
+        self.code as i32
+    }
+    fn answer(&self) -> String {
+        *TOKEN.lock().unwrap() = "".to_string();
+        self.body.clone()
+    }
+}
+
+pub async fn delete() {
+    let token = TOKEN.lock().unwrap().to_string();
+    let data = DeletePost { token: &token };
+    let url = API_URL.lock().unwrap().to_string();
+    call::<DeletePost<'_>, DeleteAnswer>(url, &data, "user", "delete").await;
+}
--- crates/cli/src/handlers/mod.rs
+++ crates/cli/src/handlers/mod.rs
@@ -1,4 +1,5 @@
 pub mod api;
+pub mod delete;
 pub mod login;
 pub mod logout;
 pub mod register;
