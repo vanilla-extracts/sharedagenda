414fc1038ae13ab5338a209d01f794688a9f5d4e
Charlotte Thomas <me@charlotte-thomas.me>
2025-04-23 12:24:42 +0200

    client: first commit of client

---
+++ crates/cli/src/configuration/loader.rs
@@ -0,0 +1,151 @@
+use ansi_term::{ANSIGenericString, Color};
+use serde::{Deserialize, Serialize};
+
+use crate::VERSION;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Greeting {
+    pub greeting_message: String,
+    pub greeting_colour: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Prompt {
+    pub prompt_message: String,
+    pub prompt_colour: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Configuration {
+    pub api_link: String,
+    pub greeting: Greeting,
+    pub prompt: Prompt,
+}
+
+#[derive(Clone)]
+pub struct Loaded<'a> {
+    pub api_link: String,
+    pub greeting_message: ANSIGenericString<'a, str>,
+    pub greeting_colour: Color,
+    pub prompt_message: String,
+    pub prompt_colour: Color,
+}
+
+impl Default for Greeting {
+    fn default() -> Self {
+        Self {
+            greeting_message:
+                "Welcome to SharedAgenda CLI REPL Version %version%, type help for help".to_string(),
+            greeting_colour: "blue".to_string(),
+        }
+    }
+}
+
+impl Default for Prompt {
+    fn default() -> Self {
+        Self {
+            prompt_message: "> ".to_string(),
+            prompt_colour: "red".to_string(),
+        }
+    }
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            api_link: "http://localhost:8000".to_string(),
+            prompt: Prompt::default(),
+            greeting: Greeting::default(),
+        }
+    }
+}
+
+pub fn load() -> Result<Configuration, confy::ConfyError> {
+    let cfg = confy::load("sharedagenda", Some("cli"));
+    match cfg {
+        Ok(config) => Ok(config),
+        _ => Ok(Configuration::default()),
+    }
+}
+
+pub fn write_config(c: &Configuration) -> Result<(), confy::ConfyError> {
+    confy::store("sharedagenda", Some("cli"), c)?;
+    Ok(())
+}
+
+pub fn write_default_config() -> Result<(), confy::ConfyError> {
+    write_config(&Configuration::default())
+}
+
+pub fn load_rgb_color(str: &str) -> (u8, u8, u8) {
+    let first = &str[0..2];
+    let second = &str[2..4];
+    let last = &str[4..6];
+
+    let rd = u8::from_str_radix(first, 16);
+    let gd = u8::from_str_radix(second, 16);
+    let bd = u8::from_str_radix(last, 16);
+
+    let r = match rd {
+        Ok(c) => c,
+        Err(_) => 0xFF,
+    };
+
+    let g = match gd {
+        Ok(c) => c,
+        Err(_) => 0xFF,
+    };
+
+    let b = match bd {
+        Ok(c) => c,
+        Err(_) => 0xFF,
+    };
+    (r, g, b)
+}
+
+pub fn load_color(string: String) -> Color {
+    match string.to_lowercase().as_str() {
+        "purple" => Color::Purple,
+        "cyan" => Color::Cyan,
+        "blue" => Color::Blue,
+        "black" => Color::Black,
+        "red" => Color::Red,
+        "yellow" => Color::Yellow,
+        "green" => Color::Green,
+        "white" => Color::White,
+        s => {
+            if s.starts_with("#") {
+                let str = s.strip_prefix("#");
+                if str.unwrap().len() < 6 {
+                    Color::Cyan
+                } else {
+                    let (r, g, b) = load_rgb_color(&str.unwrap());
+                    if r == 0xFF && g == 0xFF && b == 0xFF {
+                        Color::Cyan
+                    } else {
+                        Color::RGB(r, g, b)
+                    }
+                }
+            } else {
+                Color::Cyan
+            }
+        }
+    }
+}
+
+pub fn replace_variable(str: String) -> String {
+    str.replace("%author%", "Charlotte Thomas")
+        .replace("%version%", VERSION)
+        .to_string()
+}
+
+pub fn load_config<'a>(config: Configuration) -> Loaded<'a> {
+    Loaded {
+        greeting_colour: load_color(config.clone().greeting.greeting_colour),
+        greeting_message: load_color(config.greeting.greeting_colour)
+            .paint(replace_variable(config.greeting.greeting_message)),
+        prompt_message: config.prompt.prompt_message,
+        prompt_colour: load_color(config.prompt.prompt_colour),
+        api_link: config.api_link,
+    }
+}
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,117 @@
+use std::{
+    env::{self, Args},
+    io::{self, BufRead},
+    process::exit,
+};
+
+use ansi_term::Color;
+use atty::Stream;
+use configuration::loader::{Configuration, load, load_config, write_default_config};
+use linefeed::{Interface, ReadResult};
+
+static VERSION: &str = "v0.1.0";
+
+mod configuration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = env::args();
+    if args.len() > 1 || !atty::is(Stream::Stdin) {
+        let mut a: Vec<String> = vec![];
+
+        if !atty::is(Stream::Stdin) {
+            let stdin = io::stdin();
+            for line in stdin.lock().lines() {
+                a.push(line.unwrap());
+            }
+        } else {
+            let _ = args.next();
+            args.for_each(|f| a.push(f));
+        }
+
+        match a.first().unwrap().as_str() {
+            "-h" | "--help" => {
+                println!("-----SharedAgenda CLI Help-----");
+                println!("");
+                println!("sharedagenda              > launch the REPL");
+                println!("sharedagenda --help|-h    > show the help");
+                println!("sharedagenda --conf|-c    > prints the configuration file path");
+                println!("sharedagenda --version|-v > prints the version");
+                println!("sharedagenda api [url]    > set the URL for which API to use");
+                println!("sharedagenda register     > register a new account for sharedagenda");
+                println!("sharedagenda login        > login with your account");
+                println!("sharedagenda new|create   > create a new event");
+                println!("sharedagenda list <date>  > prints out the list of events");
+                println!("sharedagenda change       > change user information");
+                println!("sharedagenda modify       > modify event information");
+                println!("sharedagenda pretty       > pretty print the list of events");
+                println!("");
+                println!("-----SharedAgenda CLI Help-----");
+            }
+            "-v" | "--version" => {
+                println!("SharedAgenda CLI {VERSION}");
+            }
+            "-c" | "--conf" => {
+                println!("$HOME/.config/sharedagenda/cli.toml");
+            }
+            _ => {
+                println!("SOON");
+            }
+        }
+    }
+
+    let config = match load() {
+        Ok(config) => config,
+        Err(_) => {
+            let _ = write_default_config();
+            match load() {
+                Ok(cfg) => cfg,
+                Err(_) => {
+                    println!("fatal error please remove your config file");
+                    exit(1);
+                }
+            }
+        }
+    };
+
+    let loaded = load_config(config);
+
+    println!("{}", loaded.greeting_message);
+
+    let interface = Interface::new("sharedagenda").unwrap();
+    let style = &loaded.prompt_colour;
+    let prompt = &loaded.prompt_message;
+    interface
+        .set_prompt(&format!(
+            "\x01{prefix}\x02{prompt}\x01{suffix}\x02",
+            prefix = style.prefix(),
+            prompt = prompt,
+            suffix = style.suffix()
+        ))
+        .unwrap();
+
+    while let ReadResult::Input(line) = interface.read_line().unwrap() {
+        match line.as_str().trim() {
+            "version" => println!("SharedAgenda CLI REPL {VERSION}"),
+            "config" => println!("$HOME/.config/sharedagenda/cli.toml"),
+            "exit" => break,
+            "help" => {
+                println!("-----SharedAgenda CLI Help-----");
+                println!("");
+                println!("help          > show the help");
+                println!("config        > prints the configuration file path");
+                println!("version       > prints the version");
+                println!("api [url]     > set the URL for which API to use");
+                println!("register      > register a new account for sharedagenda");
+                println!("login         > login with your account");
+                println!("new|create    > create a new event");
+                println!("list <date>   > prints out the list of events");
+                println!("change        > change user information");
+                println!("modify        > modify event information");
+                println!("pretty <date> > pretty print the list of events");
+                println!("");
+                println!("-----SharedAgenda CLI Help-----");
+            }
+            _ => println!("SOON"),
+        }
+    }
 }
