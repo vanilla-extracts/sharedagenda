666310b3e2d38be67d7841eb48d1960d1c261400
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2025-04-24 14:26:31 +0200

    server: add whoami endpoint

---
+++ crates/server/src/users/whoami.rs
@@ -0,0 +1,48 @@
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+
+use super::{
+    delete::{get_token_struct_from_token, get_user_from_token},
+    structs::User,
+};
+
+extern crate rocket;
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct UserWhoami {
+    token: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct UserWhoamiAnswer {
+    code: i32,
+    user: Option<User>,
+}
+
+#[post("/user/whoami", format = "application/json", data = "<body>")]
+pub async fn whoami(body: Json<UserWhoami>) -> Json<UserWhoamiAnswer> {
+    let token = match get_token_struct_from_token(body.token.clone()).await {
+        Some(tk) => tk,
+        None => {
+            return Json(UserWhoamiAnswer {
+                code: 402,
+                user: None,
+            });
+        }
+    };
+    let user = match get_user_from_token(token).await {
+        Some(usr) => usr,
+        None => {
+            return Json(UserWhoamiAnswer {
+                code: 405,
+                user: None,
+            });
+        }
+    };
+    Json(UserWhoamiAnswer {
+        code: 200,
+        user: Some(user),
+    })
+}
