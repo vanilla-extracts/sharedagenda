e3a815aa0358e3ddc93b33d15b3875b2841db176
Charlotte Thomas <charlotte.thomas@dgfip.finances.gouv.fr>
2025-04-28 15:00:20 +0200

    psql: two databases are redundant

---
+++ 02-configuration/files/tables.sql
@@ -0,0 +1,23 @@
+create table if not exists agenda.users(
+        uuid varchar(255) primary key,
+        email varchar(50) not null,
+        name varchar(50) not null,
+        password varchar(50) not null
+);
+
+create table if not exists agenda.events(
+	id serial primary key,
+        owner varchar(255) references users(uuid) not null,
+        name varchar(255) not null,
+        date_start timestamp with time zone not null,
+        date_end timestamp with time zone not null
+);
+
+
+create table if not exists agenda.token(
+	id serial primary key,
+        token varchar(255) not null,
+        owner varchar(255) not null references users(uuid),
+        expiration_date timestamp with time zone not null
+);
+
