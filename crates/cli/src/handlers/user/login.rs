500b4ab17e4ef9d8bcfb8dad7ff604669162bba3
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-17 11:14:09 +0200

    common: move configuration in common

--- crates/cli/src/handlers/user/login.rs
+++ crates/cli/src/handlers/user/login.rs
@@ -1,9 +1,11 @@
-use common::structs::struct_user::{LoginAnswer, LoginPost};
+use common::{
+    configuration::loader::{load, write_config},
+    structs::struct_user::{LoginAnswer, LoginPost},
+};
 
 use crate::{
     API_URL, TOKEN,
     call::{Answer, call},
-    configuration::loader::{load, write_config},
 };
 
 impl Answer for LoginAnswer {
--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,1 +1,3 @@
+pub static VERSION: &str = "v3.0.0-dev";
+pub mod configuration;
 pub mod structs;
