038980b4ba6ee8047040f787ab44e603ec1f8b1b
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-17 12:48:30 +0200

    template: move everything to a template widget

--- crates/tui/src/widgets/main.rs
+++ crates/tui/src/widgets/main.rs
@@ -1,14 +1,6 @@
-use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    widgets::Widget,
-};
+use ratatui::widgets::{Paragraph, Widget};
 
-use super::{navigation_bar::NavigationBar, top::Top};
-
-pub struct MainWidget<'a> {
-    pub token: &'a str,
-    pub api_link: &'a str,
-}
+pub struct MainWidget {}
 /*
 layout:
 --------
@@ -17,25 +9,11 @@ layout:
 
 
 */
-impl Widget for MainWidget<'_> {
+impl Widget for MainWidget {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(5),
-                Constraint::Min(1),
-                Constraint::Length(3),
-            ])
-            .split(area);
-        let top_bar = Top {
-            token: self.token,
-            api_link: self.api_link,
-        };
-        top_bar.render(chunks[0], buf);
-        let navigation_bar = NavigationBar {};
-        navigation_bar.render(chunks[2], buf);
+        Paragraph::new("Hello World").render(area, buf);
     }
 }
--- crates/tui/src/widgets/mod.rs
+++ crates/tui/src/widgets/mod.rs
@@ -1,3 +1,4 @@
 pub mod main;
 pub mod navigation_bar;
+pub mod template;
 pub mod top;
