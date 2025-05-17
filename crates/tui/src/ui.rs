e893696f94a7aa1df63cb5e1b7313ad9b40731bb
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-17 17:20:02 +0200

    render: renders list as stateful widget

--- crates/tui/src/ui.rs
+++ crates/tui/src/ui.rs
@@ -9,7 +9,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
     let main_screen = TemplateWidget {
         token: app.config.token.as_str(),
         api_link: app.config.api_link.as_str(),
-        middle: MainWidget {},
+        middle: &mut MainWidget::new(),
     };
     frame.render_widget(main_screen, frame.area());
 }
--- crates/tui/src/widgets/main.rs
+++ crates/tui/src/widgets/main.rs
@@ -1,19 +1,138 @@
-use ratatui::widgets::{Paragraph, Widget};
+use ratatui::{
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Style, Stylize, palette::material::AccentedPalette},
+    text::{Line, Text},
+    widgets::{Block, List, ListItem, ListState, Paragraph, StatefulWidget, Widget, Wrap},
+};
 
-pub struct MainWidget {}
+#[derive(Clone, Debug)]
+pub struct MainWidget {
+    pub actions: ActionList,
+}
 /*
 layout:
 --------
-
+What do you wish to do?
+-> api
+-> users
+->
 
 
 
 */
-impl Widget for MainWidget {
+#[derive(Clone, Debug)]
+pub enum ActionType {
+    ChangeApiUrl,
+    RegisterAccount,
+    Login,
+    Logout,
+    Delete,
+    ModifyAccount,
+    ShowInformation,
+    ListUsers,
+    CreateEvent,
+    ShowEvents,
+}
+
+#[derive(Clone, Debug)]
+struct ActionList {
+    actions: Vec<Action>,
+    state: ListState,
+}
+
+#[derive(Clone, Debug)]
+pub struct Action {
+    pub action_type: ActionType,
+    pub action_name: String,
+}
+
+impl Action {
+    pub fn new(action_type: ActionType, name: &str) -> Self {
+        Self {
+            action_type,
+            action_name: name.to_string(),
+        }
+    }
+}
+
+impl MainWidget {
+    pub fn new() -> Self {
+        let mut new_widget = MainWidget {
+            actions: ActionList {
+                actions: vec![],
+                state: ListState::default(),
+            },
+        };
+        new_widget.actions();
+        new_widget
+    }
+    fn actions(&mut self) {
+        self.actions
+            .actions
+            .push(Action::new(ActionType::ChangeApiUrl, "Change the api url"));
+        self.actions.actions.push(Action::new(
+            ActionType::RegisterAccount,
+            "Register a new account",
+        ));
+        self.actions
+            .actions
+            .push(Action::new(ActionType::Login, "Login into your account"));
+        self.actions
+            .actions
+            .push(Action::new(ActionType::Logout, "Logout of your account"));
+        self.actions
+            .actions
+            .push(Action::new(ActionType::Delete, "Delete your account"));
+        self.actions.actions.push(Action::new(
+            ActionType::ModifyAccount,
+            "Modify your account data",
+        ));
+        self.actions.actions.push(Action::new(
+            ActionType::ShowInformation,
+            "Show your account data",
+        ));
+        self.actions
+            .actions
+            .push(Action::new(ActionType::ListUsers, "List all users"));
+        self.actions
+            .actions
+            .push(Action::new(ActionType::CreateEvent, "Create a new event"));
+        self.actions.actions.push(Action::new(
+            ActionType::ShowEvents,
+            "List all events in your calendar",
+        ));
+    }
+}
+
+impl From<&Action> for ListItem<'_> {
+    fn from(value: &Action) -> Self {
+        ListItem::new(Line::from(value.action_name.clone()).fg(Color::White))
+    }
+}
+
+impl Widget for &mut MainWidget {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
-        Paragraph::new("Hello World").render(area, buf);
+        let block = Block::bordered()
+            .title("Action")
+            .title_alignment(Alignment::Center)
+            .style(Style::default().fg(Color::Yellow));
+
+        let ls: Vec<ListItem> = self
+            .actions
+            .actions
+            .iter()
+            .map(|f| ListItem::from(f))
+            .collect();
+
+        let list = List::new(ls)
+            .block(block)
+            .style(Style::default().fg(Color::White))
+            .highlight_symbol(">")
+            .highlight_style(Style::default().fg(Color::Blue).bold());
+
+        StatefulWidget::render(list, area, buf, &mut self.actions.state);
     }
 }
