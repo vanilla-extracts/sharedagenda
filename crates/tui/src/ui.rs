d6e8dc53052c9933c7999470d3e47f6788052b98
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-17 18:22:42 +0200

    wip: debug nightmare

--- crates/tui/src/ui.rs
+++ crates/tui/src/ui.rs
@@ -1,20 +1,23 @@
 use ratatui::Frame;
 
 use crate::{
-    app::App,
-    widgets::{
-        main::MainWidget, navigation_bar::NavigationBar, template::TemplateWidget, top::Top,
-    },
+    app::{App, CurrentScreen},
+    widgets::{navigation_bar::NavigationBar, template::TemplateWidget, top::Top},
 };
 
-pub fn ui(frame: &mut Frame, app: &App) {
-    let main_screen = TemplateWidget {
-        top_bar: Top {
-            token: &app.config.token,
-            api_link: &app.config.api_link,
-        },
-        middle: &mut MainWidget::new(),
-        navigation_bar: NavigationBar::default(),
-    };
-    frame.render_widget(main_screen, frame.area());
+pub fn ui(frame: &mut Frame, app: &mut App) {
+    match app.clone().current_screen {
+        CurrentScreen::Main(mut w) => {
+            let main_screen = TemplateWidget {
+                top_bar: Top {
+                    token: &app.config.token,
+                    api_link: &app.config.api_link,
+                },
+                middle: &mut w,
+                navigation_bar: NavigationBar::default(),
+            };
+            frame.render_widget(main_screen, frame.area());
+        }
+        _ => {}
+    }
 }
