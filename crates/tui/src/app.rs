26365af07caa47fa9d6e6c9712ddda469e13e912
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-17 11:59:45 +0200

    event: start handling event (to quit)

--- crates/tui/src/app.rs
+++ crates/tui/src/app.rs
@@ -2,6 +2,7 @@ use core::panic;
 use std::{hash::DefaultHasher, io};
 
 use common::configuration::loader::{self, Loaded, load, load_config};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{DefaultTerminal, Frame};
 
 use crate::ui::ui;
@@ -78,11 +79,31 @@ impl App<'_> {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
-            //TODO: event handling
+            self.handle_events()?;
         }
         Ok(())
     }
     pub fn draw(&mut self, frame: &mut Frame) {
         ui(frame, self);
+    }
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
     }
 }
